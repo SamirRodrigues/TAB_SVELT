aaa1cf93652629c4ea0ab8ade79bd58d936ca951
SamirRodrigues <37351254+SamirRodrigues@users.noreply.github.com>
2022-05-06 21:43:28 -0300

    Add files via upload

---
+++ src/components/buttons.svelte
@@ -0,0 +1,39 @@
+<div class="centralize">
+    <button
+      class={ index === selection ? 'selected' : '' }
+      on:click={ e => clicked(index) } >
+      { buttonText }
+    </button>
+  </div>
+
+<script lang="ts">
+
+  import { createEventDispatcher } from 'svelte'
+
+  export let buttonText: string = ''
+  export let selection: number
+  export let index: number
+
+  const dispatcher = createEventDispatcher()
+
+  function clicked(value: number) {
+    dispatcher('selection', value)
+  }
+</script>
+
+<style>
+  button {
+    margin-right: 10px;
+    background-color: #ccc;
+    border: 1px solid rgb(118, 118, 118);
+    border-radius: 3px;
+    padding: 2px 5px;
+  }
+  .selected {
+    font-weight: bold;
+    background-color: #aaa;
+  }
+  .centralize {
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/components/tab.svelte
@@ -0,0 +1,66 @@
+<div  class="boxPane">
+	<div class="tabPane">
+		<div class="centralize">
+			{#each tab as tabButton, index (tabButton)}
+			<Buttons
+				buttonText={tabButton.buttonText}
+				selection={currentButton}
+				index={index}
+				on:selection={e => onSelection(e.detail)}
+			/>
+			{/each}
+		</div>
+		<h2 class="centralize">{currentButton >= 0 ? tab[currentButton].description : '' }</h2>
+	</div>
+</div>
+
+
+<script lang="ts">
+import Buttons from './buttons.svelte'
+
+const tab = [
+	{
+		description: '1',
+		buttonText: 'Tab 1'
+	},
+	{
+		description: '2',
+		buttonText: 'Tab 2'
+	},
+	{
+		description: '3',
+		buttonText: 'Tab 3'
+	},
+	{
+		description: '4',
+		buttonText: 'Tab 4'
+	}
+]
+let currentButton: number
+
+
+function onSelection(option: number) {
+  currentButton = option
+}
+
+</script>
+
+<style>
+	.boxPane {
+		display: flex;
+		justify-content: center;
+		padding-top: 50px;
+	}
+	.tabPane {
+		padding: 20px;
+		margin: 10px 0;
+		width: 300px;
+		border-radius: 10px;
+		border: 1px solid #aaa;
+		background-color: #f0f0f0;
+	}
+	.centralize {
+		display: flex;
+		justify-content: center;
+	}
+</style>
